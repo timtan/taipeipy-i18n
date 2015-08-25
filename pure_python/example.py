12b1a373545297feeb6d6c8d21dacd6a99e333b4
tim <tim.yellow@gmail.com>
2015-08-25 11:25:01 +0800

    udpate message

--- pure_python/example.py
+++ pure_python/example.py
@@ -1,12 +1,13 @@
-# -*- coding: utf-8 -*-
 #!/usr/bin/python
+# -*- coding: utf-8 -*-
 
 import gettext 
 import os.path
 
-current_dir = os.path.dirname(os.path.abspath(__file__))
 
+current_dir = os.path.dirname(os.path.abspath(__file__))
 gettext.install('example', localedir=current_dir)
 
-print( _('Hello!')) 
-print( _('World!')) 
+
+print(_('Hello!'))
+print(_('World!'))
